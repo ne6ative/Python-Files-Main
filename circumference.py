8421a6b6e265bb652f0f6c4dc4157f7fa568c420
negative <79220293+ne6ative@users.noreply.github.com>
2022-07-24 17:36:01 -0400

    Add files via upload

---
+++ circumference.py
@@ -0,0 +1,13 @@
+# First Mini Python Program :)
+# Author: twitter: @mugen
+
+pi = 3.14159
+print('Circumference Calculator :)')
+init_question = int(input("What is the radius of your circle? "))
+circ = 2 * pi * init_question
+print('The circumference of your circle is....')
+print(circ)
+
+
+
+
